92b521796ece330b2e07a1441512de2186500ba6
NoteFx <kitsu-geissler@t-online.de>
2020-12-05 14:18:42 +0100

    Update Logger.java
    added comments
    added append writing into logger file
    added synchronized parts 
    added mkdirs when default directory doesn't exist
    Logger constructor now throws IOException, if default directory can't be created

--- Logger/Logger.java
+++ Logger/Logger.java
@@ -10,28 +10,59 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class Logger extends Thread implements LoggerInt {
+/**
+ * The Logger is a custom made open source log service
+ * @author notefox (https://github.com/NoteFox)
+ */
+public class Logger extends Thread implements LoggerInterface {
 
+    // logger instance
     public static Logger instance;
 
-    private String baseFile;
-    private File currentWritingDir;
-    public File currentWritingFile;
+    // base file prefix
+    private final String baseFile;
 
+    // current writing Directory where Log Files are inserted
+    private File currentWritingDir;
+
+    // current writing File where Log Entries are inserted in
+    private File currentWritingFile;
+
+    // buffer list for Log Entries
     private List<LogEntry> BUFFER_LIST = new ArrayList<>();
 
+    // date and time formatter for log entries, directory and file names
     private final SimpleDateFormat dirFormatter = new SimpleDateFormat("yyyy-MM-dd");
     private final SimpleDateFormat fileFormatter = new SimpleDateFormat("HH-mm-ss z");
     private final SimpleDateFormat logFormatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
 
+    // initial defined variables
+    // default logging directory
     private final String logDefaultDir;
+    // max log file
     private final int maxLogFileSize;
+    // std output bit
     private final boolean logTerminalOutput;
 
+    // exit bit
     private boolean stop = false;
+
+    // info bit for logger
     private boolean isWaiting = false;
 
-    public Logger(String logDefaultDir, int maxLogFileSize, boolean logTerminalOutput) {
+    /**
+     * Logger constructor
+     * @param logDefaultDir dir where logging will take place
+     * @param maxLogFileSize max log file size
+     * @param logTerminalOutput std output bit
+     */
+    public Logger(String logDefaultDir, int maxLogFileSize, boolean logTerminalOutput) throws IOException {
+        if (!new File(logDefaultDir).exists()) {
+            boolean result = new File(logDefaultDir).mkdirs();
+            if (!result) {
+                throw new IOException("couldn't create needed directory: " + logDefaultDir);
+            }
+        }
         this.logDefaultDir = logDefaultDir;
         this.maxLogFileSize = maxLogFileSize;
         this.logTerminalOutput = logTerminalOutput;
@@ -46,20 +77,40 @@ public class Logger extends Thread implements LoggerInt {
             instance = this;
     }
 
+    /**
+     * getter for current buffer list ( not real buffer list )
+     * @return ArrayList<LogEntry>
+     */
     public List<LogEntry> getBUFFER_LIST() {
-        return BUFFER_LIST;
+        return new ArrayList<>(BUFFER_LIST);
     }
 
-    public void setBUFFER_LIST(List<LogEntry> BUFFER_LIST) {
-        this.BUFFER_LIST = BUFFER_LIST;
-    }
-
+    /**
+     * log entry adder
+     * @param type type of entry
+     * @param TAG entry tag
+     * @param message entry message
+     */
     public void addLogEntry(LogType type, String TAG, String message) {
-        BUFFER_LIST.add(new LogEntry(type, TAG, message));
+        synchronized (this) {
+            BUFFER_LIST.add(new LogEntry(type, TAG, message));
+        }
         if (isWaiting)
             this.interrupt();
     }
 
+    /**
+     * exit method for Thread
+     */
+    public void stopLogger() {
+        this.stop = true;
+        interrupt();
+    }
+
+    /**
+     * method to create and swap to next file if current file is full
+     * @throws IOException is thrown, if new file can't be created
+     */
     private void swapToNextFileIfNecessary() throws IOException {
         if (this.currentWritingFile.length() / 1000 > maxLogFileSize) {
             this.currentWritingFile = new File(currentWritingDir + "/" + baseFile.replace(".txt", "") + "["+ fileFormatter.format(System.currentTimeMillis()) +"].txt");
@@ -69,6 +120,10 @@ public class Logger extends Thread implements LoggerInt {
         }
     }
 
+    /**
+     * method to create and swap to next directory if new entry into file happens in next day
+     * @throws IOException
+     */
     private void swapToNextDayDirIfNecessary() throws IOException {
         Date date = new Date(System.currentTimeMillis());
 
@@ -77,7 +132,7 @@ public class Logger extends Thread implements LoggerInt {
 
             if (!this.currentWritingDir.exists())
                 if (!this.currentWritingDir.mkdir()) {
-                    java.lang.System.out.println("couldn't create new writing dir");
+                    throw new IOException("couldn't create new writing dir");
                 }
 
             this.currentWritingFile = new File(currentWritingDir + "/" + baseFile.replace(".txt", "") + "["+ fileFormatter.format(System.currentTimeMillis()) +"].txt");
@@ -87,12 +142,9 @@ public class Logger extends Thread implements LoggerInt {
         }
     }
 
-    @Override
-    public void stopThread() {
-        this.stop = true;
-        interrupt();
-    }
-
+    /**
+     * run
+     */
     @Override
     public synchronized void run() {
         do {
@@ -107,21 +159,23 @@ public class Logger extends Thread implements LoggerInt {
                 e.printStackTrace();
             }
 
-            if (BUFFER_LIST.isEmpty()) {
-                isWaiting = true;
-                try {
-                    wait();
-                } catch (InterruptedException e) {
-                    // kindly ignore
+            synchronized (this) {
+                if (BUFFER_LIST.isEmpty()) {
+                    isWaiting = true;
+                    try {
+                        wait();
+                    } catch (InterruptedException e) {
+                        // kindly ignore
+                    }
+                    isWaiting = false;
+                } else {
+                    try {
+                        writeEntry(BUFFER_LIST.get(0));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    BUFFER_LIST.remove(0);
                 }
-                isWaiting = false;
-            } else {
-                try {
-                    writeEntry(BUFFER_LIST.get(0));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                BUFFER_LIST.remove(0);
             }
 
             if (stop)
@@ -129,29 +183,34 @@ public class Logger extends Thread implements LoggerInt {
         } while (true);
     }
 
+    /**
+     * entry writer into current file
+     * @param entry entry to write in
+     * @throws IOException is thrown, if current file can't be accessed
+     */
     private void writeEntry(LogEntry entry) throws IOException {
         String temp = "";
 
-        if (!currentWritingFile.exists())
-            currentWritingFile.createNewFile();
-
         Date date = new Date(System.currentTimeMillis());
-        String input = entry.type + " | " + logFormatter.format(date) + " | " + entry.TAG + " : " + entry.message;
+        String input = entry.getType() + " | " +
+                logFormatter.format(date) + " | " + entry.getTAG() + " : " + entry.getMessage();
 
         if (logTerminalOutput)
             java.lang.System.out.println(input);
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(currentWritingFile)));
+        synchronized (this) {
+            /* BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(currentWritingFile)));
 
-        while(br.ready()) {
-            temp += br.readLine() + "\n";
+            while (br.ready()) {
+                temp += br.readLine() + "\n";
+            }
+
+            temp += input + "\n";
+            br.close(); */
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter(currentWritingFile, true));
+            bw.write(input + "\n");
+            bw.close();
         }
-
-        temp += input + "\n";
-        br.close();
-
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(currentWritingFile)));
-        bw.write(temp);
-        bw.close();
     }
 }
