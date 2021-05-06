6f4a35d0d86c1b3ccbc844ef146026002c948866
NoteFx <kitsu-geissler@t-online.de>
2021-05-06 23:17:05 +0200

    Update railsInstaller_debian.sh

--- railsInstaller_debian.sh
+++ railsInstaller_debian.sh
@@ -5,17 +5,17 @@
 #install ruby 3.0.1
 sudo apt purge ruby -y && sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y
 sudo apt install git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential lolcat libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev -y
-curl -sL https://raw.githubusercontent.com/rbenv/rbenv-installer/main/bin/rbenv-installer | bash - | lolcat
+curl -sL https://raw.githubusercontent.com/rbenv/rbenv-installer/main/bin/rbenv-installer | bash -
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 exec $SHELL
-rbenv install -l | lolcat
-rbenv install 3.0.1 | lolcat
-rbenv global 3.0.1 | lolcat
-ruby -v | lolcat
+rbenv install -l
+rbenv install 3.0.1
+rbenv global 3.0.1
+ruby -v
 
 #install rails
-sudo apt install sqlite3 npm yarn -y | lolcat
-gem install rails | lolcat
-rails --version | lolcat
+sudo apt install sqlite3 npm yarn -y
+gem install rails
+rails --version
 print "done"
